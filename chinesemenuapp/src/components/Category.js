a79bcfd29cb515635b3e1e46fd66582228388685
Parth Vijayvargiya <parthvijayvargiyia@gmail.com>
2018-09-10 17:58:54 -0700

    fixed indent

--- chinesemenuapp/src/components/Category.js
+++ chinesemenuapp/src/components/Category.js
@@ -1,49 +1,48 @@
 import React, { Component } from "react";
 import axios from 'axios';
 
-
 class Category extends Component {
   state = {
     items: []
   };
 
-  componentDidMount() {
-    axios.get('https://davids-restaurant.herokuapp.com/categories.json').then(
-        (res) => {
-            this.setState({
-                items: res.data
-            })
-        },
-        (error) => {
-            console.log(error);
-        })
-  }
-
-  getDetails(i) {
-      this.props.history.push({ pathname: '/categories/items/' + i.short_name});
-  }
-
-  render() {
-      let menuItems = this.state.items.map( (item) => {
-          return <li 
-            key={item.id}
-            onClick={() => {this.getDetails(item)}}
-          >
-            {item.name} - ({item.short_name})
-        </li>;
+componentDidMount() {
+  axios.get('https://davids-restaurant.herokuapp.com/categories.json').then(
+      (res) => {
+          this.setState({
+              items: res.data
+          })
+      },
+      (error) => {
+          console.log(error);
       })
-    const style={
-        float: 'left'
-    }
-    return (
-        <div style={style}>
-        <h3>Menu Categories</h3>
-        <ul>
-          {menuItems}
-        </ul>
-      </div>
-    );
+}
+
+getDetails(i) {
+    this.props.history.push({ pathname: '/categories/items/' + i.short_name});
+}
+
+render() {
+    let menuItems = this.state.items.map( (item) => {
+        return <li 
+          key={item.id}
+          onClick={() => {this.getDetails(item)}}
+        >
+          {item.name} - ({item.short_name})
+      </li>;
+    })
+  const style={
+      float: 'left'
   }
+  return (
+    <div style={style}>
+      <h3>Menu Categories</h3>
+      <ul>
+        {menuItems}
+      </ul>
+    </div>
+  );
+}
 }
 
 export default Category;
