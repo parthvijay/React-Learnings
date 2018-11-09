ed8d779e5479a3de4b36ef28a59398cbf1b212b5
Parth Vijayvargiya <parthvijayvargiyia@gmail.com>
2018-11-09 13:27:03 -0800

    fixes

--- componentinteraction/src/App.js
+++ componentinteraction/src/App.js
@@ -6,13 +6,22 @@ import UserInput from "./UserInput/UserInput";
 
 class App extends Component {
   state = {
-    firstName: "Parth",
-    lastName: "Vijayvargiya"
-  };
+    currentMeal: {
+      base: "rice",
+      meat: "chicken",
+      dessert: "ice cream"
+    }
+  }
 
   lastNameHandler = (newLastName) => {
-    this.setState({ lastName: newLastName });
-    console.log('Handler called');
+    const newObj = {...this.state.currentMeal};
+    console.log(newObj);
+    newObj.dessert = 'Apple Pie';
+    // this.setState({
+    //   currentMeal: { ...this.state.currentMeal, dessert: "Apple Pie" }
+    // });
+    this.setState({ currentMeal: newObj });
+    console.log(newObj);
   }
 
   lastNameChangedHandler = (event) => {
@@ -22,9 +31,12 @@ class App extends Component {
 
   render() {
     return <div className="App">
-        <button onClick={this.lastNameHandler.bind(this, 'Vijay')}>Call Me</button>
-        <UserOutput firstName={this.state.firstName} lastName={this.state.lastName} changeLastName={this.lastNameHandler.bind(this, 'Agrawal')} />
-        <UserInput changed={this.lastNameChangedHandler}/>
+        <button onClick={this.lastNameHandler}>
+          Call Me
+        </button>
+      {this.state.currentMeal.dessert}
+        <UserOutput firstName={this.state.currentMeal.dessert} lastName={this.state.currentMeal.base} changeLastName={this.lastNameHandler.bind(this, "Agrawal")} />
+        <UserInput changed={this.lastNameChangedHandler} />
       </div>;
   }
 }
